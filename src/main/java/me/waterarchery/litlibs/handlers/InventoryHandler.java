fb9553c98cfb269d1c51ac47c966e5bbc65cca87
WaterArchery <loljoket@gmail.com>
2024-07-23 17:10:55 +0300

    Added custom model data support and custom naming support on fill item.

--- src/main/java/me/waterarchery/litlibs/handlers/InventoryHandler.java
+++ src/main/java/me/waterarchery/litlibs/handlers/InventoryHandler.java
@@ -25,13 +25,17 @@ public class InventoryHandler {
         boolean isFillEnabled = yaml.getBoolean(path + ".fillMenu.enabled", false);
         if (isFillEnabled) {
             String rawMaterial = yaml.getString(path + ".fillMenu.fillItem", "STONE");;
+            String itemName = yaml.getString(path + ".fillMenu.itemName", "§7");;
+            int modelData = yaml.getInt(path + ".fillMenu.customModelData", -1);;
             Optional<XMaterial> optMaterial = XMaterial.matchXMaterial(rawMaterial);
             XMaterial material = optMaterial.orElse(XMaterial.STONE);
             ItemStack itemStack = material.parseItem();
             itemStack = setGUIAction(itemStack, "none", ActionType.FILL, file);
 
             ItemMeta meta = itemStack.getItemMeta();
-            meta.setDisplayName("§7");
+            meta.setDisplayName(itemName.replace("&", "§"));
+            if (modelData != -1)
+                meta.setCustomModelData(modelData);
             itemStack.setItemMeta(meta);
 
             for (int slot = 0; slot < inventory.getSize(); slot++) {
