177ca51a7899ce55aa9950ec1e0a008eab9d185e
Arda <loljoket@gmail.com>
2025-03-14 11:44:28 +0300

    added new chunk method

--- src/main/java/me/waterarchery/litlibs/utils/ChunkUtils.java
+++ src/main/java/me/waterarchery/litlibs/utils/ChunkUtils.java
@@ -1,8 +1,18 @@
 package me.waterarchery.litlibs.utils;
 
+import org.bukkit.Location;
 import org.bukkit.World;
 
 public class ChunkUtils {
+
+    public static boolean isChunkLoaded(Location location) {
+        if (!location.isWorldLoaded()) return false;
+
+        int chunkX = location.getBlockX() >> 4;
+        int chunkZ = location.getBlockZ() >> 4;
+
+        return location.getWorld().isChunkLoaded(chunkX, chunkZ);
+    }
 
     public static boolean isChunkLoaded(World world, int chunkX, int chunkZ) {
         if (world == null) return false;
