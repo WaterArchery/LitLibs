6bf3580c35e610aecb0b3a1e70dda8bc3453ba18
Arda <loljoket@gmail.com>
2025-03-14 11:43:24 +0300

    changed entity handling

--- src/main/java/me/waterarchery/litlibs/handlers/NPCHandler.java
+++ src/main/java/me/waterarchery/litlibs/handlers/NPCHandler.java
@@ -1,27 +1,29 @@
 package me.waterarchery.litlibs.handlers;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import lombok.Getter;
 import lombok.Setter;
-import me.waterarchery.litlibs.impl.npc.types.MobNPC;
 import me.waterarchery.litlibs.impl.npc.NPC;
-import me.waterarchery.litlibs.impl.npc.types.PlayerNPC;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.entity.EntityType;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 @Getter
 @Setter
 public class NPCHandler {
 
     private static NPCHandler instance;
-    final List<NPC> npcs = new ArrayList<>();
+    private final List<NPC> npcs = new ArrayList<>();
+    private final ThreadFactory namedThreadFactory;
+    private final ExecutorService executor;
 
     public static NPCHandler getInstance() {
         if (instance == null) instance = new NPCHandler();
@@ -29,7 +31,10 @@ public class NPCHandler {
         return instance;
     }
 
-    private NPCHandler() { }
+    private NPCHandler() {
+        namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("litlibs-%d").build();
+        executor = Executors.newCachedThreadPool(namedThreadFactory);
+    }
 
     public void deleteNPC(UUID uuid) {
         List<NPC> deletedNpcs = npcs.stream().filter(npc -> npc.getUuid().equals(uuid)).toList();
